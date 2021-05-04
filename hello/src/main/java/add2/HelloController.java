81cfb63431c897962b4bbedb690d429696bf50b3
karthikeya <karthikeya@Eagle>
2021-05-04 20:08:29 +0530

    Commit message i have changed this one

--- hello/src/main/java/add2/HelloController.java
+++ hello/src/main/java/add2/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.ModelMap;
 @RequestMapping("/hello")
 public class HelloController {
    @RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
-      model.addAttribute("message", "hbefgutltrk MVC Framework!");
+      model.addAttribute("message", "I HAVE changed this MVC Framework!");
       return "hello";
    }
 }
